086b1ea7e4cd128a83d45dda0b8c8b35a2d9fa9a
Anastasia Fedorenko <anastasia.fed2009@gmail.com>
2026-07-05 10:55:55 +0300

    Cleaning session

--- SBThub.Application/UseCases/Guests/GetGuests/GetGuestsQuery.cs
+++
@@ -1,7 +0,0 @@
-using HotelDemo.Application.Abstractions.Messaging;
-using HotelDemo.Application.Contracts.Responses;
-
-namespace HotelDemo.Application.UseCases.Guests.GetGuests;
-
-/// <summary>Запрос: получить список всех гостей.</summary>
-public sealed record GetGuestsQuery : IQuery<IReadOnlyList<GuestResponse>>;
--- SBThub.Infrastructure/obj/Debug/net10.0/SBThub.Infrastructure.AssemblyInfo.cs
+++ SBThub.Infrastructure/obj/Debug/net10.0/SBThub.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SBThub.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4e47b6a9943337067cc110d1531458967e3ddbf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a91d6b3700a9ee64ad000758d6636f67f5afc2a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("SBThub.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SBThub.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
