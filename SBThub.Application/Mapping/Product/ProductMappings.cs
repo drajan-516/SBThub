5a4e7d77321d72397fd0d11d02f8dfb165609844
Anastasia Fedorenko <anastasia.fed2009@gmail.com>
2026-07-10 02:12:15 +0300

    feat(mapping): added mapping for product

---
+++ SBThub.Application/Mapping/Product/ProductMappings.cs
@@ -0,0 +1,9 @@
+﻿using SBThub.Application.Contracts.Responses;
+
+namespace SBThub.Application.Mapping.Product;
+
+public static class ProductMappings
+{
+    public static ProductResponse ToResponse(this SBThub.Domain.Entities.Product product) =>
+        new(product.Uuid, product.FullTitle, product.Description, product.Price, product.CreatedOn);
+}
