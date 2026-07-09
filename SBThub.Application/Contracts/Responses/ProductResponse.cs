f12ad7fc155758c433641afd72969366eb988c0a
Anastasia Fedorenko <anastasia.fed2009@gmail.com>
2026-07-10 02:14:11 +0300

    feat(response): create response for product

---
+++ SBThub.Application/Contracts/Responses/ProductResponse.cs
@@ -0,0 +1,4 @@
+﻿namespace SBThub.Application.Contracts.Responses;
+
+//do we need creator id here??
+public sealed record ProductResponse(Guid Uuid, string ProductTitle, string Description,  decimal Price, DateTime CreatedOn);
