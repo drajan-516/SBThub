d154f841745066f93514128df9988d0db625a06e
Anastasia Fedorenko <anastasia.fed2009@gmail.com>
2026-07-12 00:51:03 +0300

    feat: product errors

---
+++ SBThub.Domain/Errors/ProductErrors.cs
@@ -0,0 +1,16 @@
+﻿using SBThub.Domain.Shared;
+using SBThub.Domain.ValueObjects.Product;
+
+namespace SBThub.Domain.Errors;
+
+public static class ProductErrors
+{
+    public static readonly Error NameRequired =
+        Error.Validation("Product.NameRequired", "Название продукта обязательно.");
+
+    public static readonly Error NameTooLong =
+        Error.Validation("Product.NameTooLong", $"Название продукта не длиннее {ProductTitle.MaxLength} символов.");
+
+    public static readonly Error NotFound =
+        Error.NotFound("Product.NotFound", "Продукт не найден.");
+}
