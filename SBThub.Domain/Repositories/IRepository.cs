8750a66d2fe872cac847a2407148b169b5859473
Anastasia Fedorenko <anastasia.fed2009@gmail.com>
2026-07-30 00:22:48 +0300

    feat: added base repository

---
+++ SBThub.Domain/Repositories/IRepository.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq.Expressions;
+using SBThub.Domain.Common;
+
+namespace SBThub.Domain.Repositories;
+
+public interface IRepository
+{
+    Task<TEntity?> GetByUuidAsync<TEntity>(
+        Guid uuid,
+        CancellationToken ct,
+        params Expression<Func<TEntity, object?>>[] includes)
+        where TEntity : Entity;
+
+    Task<TEntity?> GetSingleAsync<TEntity>(
+        Expression<Func<TEntity, bool>> predicate,
+        CancellationToken ct = default,
+        params Expression<Func<TEntity, object?>>[] includes)
+        where TEntity : class;
+
+    Task Add<TEntity>(TEntity entity) where TEntity : Entity;
+
+    Task Add<TEntity>(TEntity entity, CancellationToken ct) where TEntity : Entity;
+
+    void Delete<TEntity>(TEntity entity) where TEntity : Entity;
+
+    void Update<TEntity>(TEntity entity) where TEntity : Entity;
+
+    Task<bool> AnyAsync<TEntity>(
+        Expression<Func<TEntity, bool>> predicate,
+        CancellationToken ct = default)
+        where TEntity : class;
+
+    Task<bool> AnyIgnoringFiltersAsync<TEntity>(
+        Expression<Func<TEntity, bool>> predicate,
+        CancellationToken ct = default)
+        where TEntity : class;
+}
---
+++ SBThub.Infrastructure/Persistence/Repositories/BaseRepository.cs
@@ -0,0 +1,83 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using SBThub.Domain.Shared;
+using System.Linq.Expressions;
+using SBThub.Domain.Common;
+using SBThub.Domain.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace SBThub.Infrastructure.Persistence.Repositories;
+
+internal sealed class BaseRepository(ShopDbContext context) : IRepository
+{
+    private ShopDbContext Context => context;
+
+    public async Task<TEntity?> GetByUuidAsync<TEntity>(
+        Guid uuid,
+        CancellationToken ct,
+        params Expression<Func<TEntity, object?>>[] includes)
+        where TEntity : Entity
+    {
+        IQueryable<TEntity> query = Context.Set<TEntity>();
+
+        query = includes.Aggregate(query, (current, include)
+            => current.Include(include));
+
+        return await query.FirstOrDefaultAsync(e => e.Uuid == uuid, ct);
+    }
+
+    public async Task<TEntity?> GetSingleAsync<TEntity>(
+        Expression<Func<TEntity, bool>> predicate,
+        CancellationToken ct = default,
+        params Expression<Func<TEntity, object?>>[] includes)
+        where TEntity : class
+    {
+        IQueryable<TEntity> query = Context.Set<TEntity>();
+
+        query = includes.Aggregate(query, (current, include)
+            => current.Include(include));
+
+        return await query.FirstOrDefaultAsync(predicate, ct);
+    }
+
+    public async Task Add<TEntity>(TEntity entity) where TEntity : Entity
+    {
+        await Context.Set<TEntity>().AddAsync(entity);
+    }
+
+    public async Task Add<TEntity>(TEntity entity, CancellationToken ct) where TEntity : Entity
+    {
+        await Context.Set<TEntity>().AddAsync(entity, ct);
+    }
+
+    public void Delete<TEntity>(TEntity entity) where TEntity : Entity
+    {
+        Context.Set<TEntity>().Remove(entity);
+    }
+
+    public void Update<TEntity>(TEntity entity) where TEntity : Entity
+    {
+        Context.Set<TEntity>().Update(entity);
+    }
+
+    public async Task<bool> AnyAsync<TEntity>(
+        Expression<Func<TEntity, bool>> predicate,
+        CancellationToken ct = default)
+        where TEntity : class
+    {
+        return await Context.Set<TEntity>()
+            .AsNoTracking()
+            .AnyAsync(predicate, ct);
+    }
+
+    public async Task<bool> AnyIgnoringFiltersAsync<TEntity>(
+        Expression<Func<TEntity, bool>> predicate,
+        CancellationToken ct = default)
+        where TEntity : class
+    {
+        return await Context.Set<TEntity>()
+            .IgnoreQueryFilters()
+            .AsNoTracking()
+            .AnyAsync(predicate, ct);
+    }
+}
