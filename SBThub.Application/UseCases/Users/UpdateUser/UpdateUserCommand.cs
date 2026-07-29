030a370d1c9a5b8f010e5d882f260f9d33803e92
Anastasia Fedorenko <anastasia.fed2009@gmail.com>
2026-07-30 01:15:29 +0300

    fix: deleted UpdateUser

--- SBThub.Application/UseCases/Users/UpdateUser/UpdateUserCommand.cs
+++
@@ -1,8 +0,0 @@
-using SBThub.Application.Abstractions.Messaging;
-using SBThub.Application.Contracts.Requests;
-using SBThub.Application.Contracts.Requests.User;
-using SBThub.Application.Contracts.Responses;
-
-namespace SBThub.Application.UseCases.Users.UpdateUser;
-
-public sealed record UpdateUserCommand(Guid Uuid, UpdateUserRequest Request) : ICommand<UserResponse>;
--- SBThub.WebApi/Controllers/UsersController.cs
+++ SBThub.WebApi/Controllers/UsersController.cs
@@ -1,7 +1,6 @@
 using SBThub.Application.Contracts.Requests;
 using SBThub.Application.UseCases.Users.CreateUser;
 using SBThub.Application.UseCases.Users.GetUsers;
-using SBThub.Application.UseCases.Users.UpdateUser;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using SBThub.Application.Contracts.Requests.User;
@@ -22,13 +21,6 @@ public sealed class UsersController(ISender sender) : BaseApiController(sender)
     public async Task<IActionResult> GetUsers(CancellationToken cancellationToken)
     {
         var result = await Sender.Send(new GetUsersQuery(), cancellationToken);
-        return result.IsFailure ? HandleFailure(result) : Ok(result.Value);
-    }
-    
-    [HttpPut("{uuid:guid}")]
-    public async Task<IActionResult> UpdateUser(Guid uuid, [FromBody] UpdateUserRequest request, CancellationToken cancellationToken)
-    {
-        var result = await Sender.Send(new UpdateUserCommand(uuid, request), cancellationToken);
         return result.IsFailure ? HandleFailure(result) : Ok(result.Value);
     }
 }
