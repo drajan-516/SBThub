bffde57327b78a6205374c645577fbac36208c88
Anastasia Fedorenko <anastasia.fed2009@gmail.com>
2026-08-20 13:30:40 +0300

    feat: add controllet for user profile

---
+++ SBThub.WebApi/Controllers/UsersProfilesController.cs
@@ -0,0 +1,25 @@
+﻿using SBThub.Application.Contracts.Requests.UserProfile;
+using SBThub.Application.UseCases.UsersProfiles.CreateUserProfile;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+
+
+namespace SBThub.WebApi.Controllers;
+
+[Route("api/usersProfiles")]
+public sealed class UsersProfilesController(ISender sender) : BaseApiController(sender)
+{
+    [HttpPost("{userId:guid}")]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status409Conflict)]
+    [SwaggerResponse(201, "User profile was successfully created.")]
+    [SwaggerResponse(400, "Invalid request. Ensure the provided data is correct.", typeof(CreateUserProfileRequest))]
+    [SwaggerResponse(409, "Conflict. User Profile with similar details already exists.", typeof(CreateUserProfileRequest))]
+    public async Task<IActionResult> CreateUserProfile(Guid userId, [FromBody] CreateUserProfileRequest request, CancellationToken cancellationToken)
+    {
+        var result = await Sender.Send(new CreateUserProfileCommand(userId, request), cancellationToken);
+        return result.IsFailure ? HandleFailure(result) : Ok(result.Value);
+    }
+}
